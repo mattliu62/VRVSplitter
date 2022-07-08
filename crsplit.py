0ea3a9e34e14e8e12e581f26b193d20fea725e35
Matthew Liu <mattliu62@gmail.com>
2022-07-08 07:41:33 -0700

    Added comments and adjusted writing location

--- crsplit.py
+++ crsplit.py
@@ -4,19 +4,34 @@ import os
 from ydl_ops import ydl_opts
 
 #Source: https://stackoverflow.com/questions/33311153/python-extracting-and-saving-video-frames
-def process(fps):
+def process(fps, path):
+    #use OpenCV video capture function
     vidcap = cv2.VideoCapture('video.mp4')
     success,image = vidcap.read()
     count = 0
+    #for each frame successfully read
     while success:
+        #fps dictates the number of frames generated per each second
         if (count % fps  == 0):
-            cv2.imwrite("frame-%d.png" % count, image)     # save frame as PNG file      
+            #we write to the image file to the specified path as a PNG
+            cv2.imwrite(os.path.join(path,"frame-%d.png" % count), image) 
         success,image = vidcap.read()
         count += 1
 
+#initializing function that prepares directory for processing
+def initialize():
+    #we check to see if a folder called 'frames' exists. If it doesn't, we create it. If it does, we clear it
+    path = os.getcwd() + '/frames'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for i in os.listdir(path):
+        os.remove(os.path.join(path, i))
+    return path
+    
 def run():
     print('           _______________________________\n /\\__/\\   / Video Frame Splitter for VRV  |\n( ° □ °) <  Developed by mattliu62 (2022) |\n(      )  \\_______________________________|')
     print('--------------------------------------------------')
+    #checks to ensure link validity
     link = input('Enter valid VRV Link: ')
     link = link.strip()
     if 'vrv' not in link:
@@ -25,10 +40,15 @@ def run():
     fps = input('Enter video FPS: ')
     fps = 30 / 1
     print('Initializing Download. Hang tight, this will take a while!')
+    #utilize youtube_dl to download as MP4
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link])
     print('Download complete! Initializing processing.')
-    process(int(fps))
+    #initialize the directory
+    path = initialize()
+    #process the video into frames, passing in the desired frames and CWD
+    process(int(fps), path)
+    #remove the MP4 file
     os.remove('video.mp4')
     print('Successfully generated frames.')
     print('Mission Complete!')
